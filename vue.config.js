c3ff171ff9ff10655012a00e5612da194aa90a3b
René Mueller <rene@renemueller.eu>
2022-06-16 14:28:20 +0200

    deployable to aws

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'eu-central-1',
+      bucket: 'taximeter.renemueller.eu',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: true,
+      pwaFiles: 'index.html,service-worker.js,manifest.json',
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
+})

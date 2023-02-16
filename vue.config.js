08f4a530f3e889f623e6ea6648548f4f186f07fa
René Mueller <rene@renemueller.eu>
2023-02-16 21:14:45 +0100

    deployment via Amplify instead of S3

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,2 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  pluginOptions: {
-    s3Deploy: {
-      registry: undefined,
-      awsProfile: 'default',
-      overrideEndpoint: false,
-      region: 'eu-central-1',
-      bucket: 'taximeter.renemueller.eu',
-      createBucket: false,
-      staticHosting: true,
-      staticIndexPage: 'index.html',
-      staticErrorPage: 'index.html',
-      assetPath: 'dist',
-      assetMatch: '**',
-      deployPath: '/',
-      acl: 'public-read',
-      pwa: true,
-      pwaFiles: 'index.html',
-      enableCloudfront: false,
-      pluginVersion: '4.0.0-rc3',
-      uploadConcurrency: 5
-    }
-  }
-})
+module.exports = defineConfig()
